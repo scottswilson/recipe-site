75d1b67baa3f09864b9dde043744657cf2836663
Scott Wilson <scottswilson2@gmail.com>
2025-09-17 23:42:25 -0600

    Added search

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import List from '@mui/material/List';
@@ -6,7 +8,7 @@ import ListItemText from '@mui/material/ListItemText';
 
 import styled from "styled-components";
 
-import {viewStyle} from "./Styled"
+import { viewStyle } from "./Styled"
 
 const SearchBox = styled(Box)`
   padding-left: 30px;
@@ -16,21 +18,62 @@ const SearchBox = styled(Box)`
 `;
 
 function RecipeList(props) {
+
+  const [searchText, setSearchText] = useState("");
+
   const { recipes, index, setIndex, setTab } = props;
   const changeRecipe = (i) => {
     setIndex(i);
     setTab(1);
   };
+
+  const numberedRecipes = recipes.map((recipe, i) => {
+    return {
+      "index": i,
+      ...recipe
+    };
+  })
+
+  const [visibleRecipes, setVisibleRecipes] = useState(numberedRecipes);
+
+  const updateSearch = (newText) => {
+    setSearchText(newText);
+    const searchText = newText.toLowerCase();
+    const filteredRecipes = numberedRecipes.filter((recipe) => {
+      const inName = recipe.name.toLowerCase().includes(searchText);
+      const inIngredients = recipe.ingredients.some(ingredient => {
+        return ingredient.label.toLowerCase().includes(searchText);
+      });
+      const inProcedure = recipe.procedure.some(step => {
+        return step.toLowerCase().includes(searchText);
+      });
+      const inTags = recipe.tags.some(tag => {
+        return tag.toLowerCase().includes(searchText);
+      });
+      return inName | inIngredients | inProcedure | inTags;
+    });
+    setVisibleRecipes(filteredRecipes);
+  };
+
   return (
     <Box style={viewStyle}>
       <SearchBox>
-        <TextField id="standard-basic" label="Search" type="search" fullWidth />
+        <TextField
+          id="standard-basic"
+          label="Search"
+          type="search"
+          fullWidth
+          value={searchText}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            updateSearch(event.target.value);
+          }}
+        />
       </SearchBox>
       <List component="nav" aria-label="recipes">
-        {recipes.map((recipe, i) => (
+        {visibleRecipes.map(recipe => (
           <ListItemButton
-            selected={index === i}
-            onClick={() => changeRecipe(i)}
+            selected={index === recipe.index}
+            onClick={() => changeRecipe(recipe.index)}
           >
             <ListItemText primary={recipe.name} />
           </ListItemButton>
